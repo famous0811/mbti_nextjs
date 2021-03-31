b53161a0429d076f1ab5aa60f98fde249c91e802
famous0811 <umh0811@naver.com>
2021-03-31 09:54:37 +0900

    exampage

--- components/examItem.tsx
+++ components/examItem.tsx
@@ -1,7 +1,79 @@
 import React from "react";
+import styles from "style/examineItem.scss";
 
-const examItem = () => {
-  return <div></div>;
+interface ExamItem {
+  title: string;
+  activation: boolean;
+  click?: string;
+}
+const examItem = ({ title, activation, click }: ExamItem) => {
+  return (
+    <div
+      className={[
+        activation ? styles.activtion : styles.disabled,
+        styles.examitem,
+      ].join(" ")}
+    >
+      <div className={styles.question}>
+        <span>{title}</span>
+      </div>
+
+      <div className={styles.decision}>
+        <div className={styles.agreed}>동의</div>
+        <div className={styles.options}>
+          <div
+            className={[
+              styles.max,
+              styles.agree,
+              click === "agree-max" ? styles.click : "",
+            ].join(" ")}
+          />
+          <div
+            className={[
+              styles.med,
+              styles.agree,
+              click === "agree-med" ? styles.click : "",
+            ].join(" ")}
+          />
+          <div
+            className={[
+              styles.min,
+              styles.agree,
+              click === "agree-min" ? styles.click : "",
+            ].join(" ")}
+          />
+          <div
+            className={[
+              styles.neutral,
+              click === "neutral" ? styles.click : "",
+            ].join(" ")}
+          />
+          <div
+            className={[
+              styles.min,
+              styles.disagree,
+              click === "disagree-min" ? styles.click : "",
+            ].join(" ")}
+          />
+          <div
+            className={[
+              styles.med,
+              styles.disagree,
+              click === "disagree-med" ? styles.click : "",
+            ].join(" ")}
+          />
+          <div
+            className={[
+              styles.max,
+              styles.disagree,
+              click === "disagree-max" ? styles.click : "",
+            ].join(" ")}
+          />
+        </div>
+        <div className={styles.disagreed}>비동의</div>
+      </div>
+    </div>
+  );
 };
 
 export default examItem;
